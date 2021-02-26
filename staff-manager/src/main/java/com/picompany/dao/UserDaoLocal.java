2a596f4a61c3668b510d5818f6dad70e609f15d4
Lucas Oliveira <lucasoliveirabs95@gmail.com>
2021-02-26 13:37:40 -0300

    add 'authorizeLogin()' method in 'UserDaoLocal' business interface

--- staff-manager/src/main/java/com/picompany/dao/UserDaoLocal.java
+++ staff-manager/src/main/java/com/picompany/dao/UserDaoLocal.java
@@ -2,7 +2,11 @@ package com.picompany.dao;
 
 import javax.ejb.Local;
 
+import com.picompany.model.User;
+
 @Local
 public interface UserDaoLocal {
+	
+	User authorizeLogin(String email, String password);
 
 }
