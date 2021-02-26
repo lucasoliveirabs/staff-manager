766be2762ca5dec94dba874faaa74326245c95e0
Lucas Oliveira <lucasoliveirabs95@gmail.com>
2021-02-26 13:49:22 -0300

    set transaction PersistenceContext to EntityManager (MySQL PU)

--- staff-manager/src/main/java/com/picompany/dao/UserDao.java
+++ staff-manager/src/main/java/com/picompany/dao/UserDao.java
@@ -1,11 +1,16 @@
 package com.picompany.dao;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import com.picompany.model.User;
 
 @Stateless
 public class UserDao implements UserDaoLocal{
+	
+	@PersistenceContext(unitName = "picompanyMySqlPU")
+	private EntityManager manager;
 
 	public User authorizeLogin(String email, String password) {
 		return null;
