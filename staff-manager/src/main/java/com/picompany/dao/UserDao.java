f048e2c662157fc25dff11672ac03314fc290d13
Lucas Oliveira <lucasoliveirabs95@gmail.com>
2021-02-26 13:38:43 -0300

    implement 'authorizeLogin()' method in 'UserDao' stateless session bean

--- staff-manager/src/main/java/com/picompany/dao/UserDao.java
+++ staff-manager/src/main/java/com/picompany/dao/UserDao.java
@@ -2,7 +2,14 @@ package com.picompany.dao;
 
 import javax.ejb.Stateless;
 
+import com.picompany.model.User;
+
 @Stateless
 public class UserDao implements UserDaoLocal{
+
+	public User authorizeLogin(String email, String password) {
+		return null;
+	}
+
 	
 }
