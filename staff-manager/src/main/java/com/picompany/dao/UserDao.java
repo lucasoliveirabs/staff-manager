6815d63e17591f3a70dec21e4256dffc798d9a24
Lucas Oliveira <lucasoliveirabs95@gmail.com>
2021-02-27 19:23:43 -0300

    add 'login' EntityManager persistence operations in 'UserDao'

--- staff-manager/src/main/java/com/picompany/dao/UserDao.java
+++ staff-manager/src/main/java/com/picompany/dao/UserDao.java
@@ -2,19 +2,34 @@ package com.picompany.dao;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import com.picompany.model.User;
 
 @Stateless
-public class UserDao implements UserDaoLocal{
-	
+public class UserDao implements UserDaoLocal {
+
 	@PersistenceContext(unitName = "picompanyMySqlPU")
 	private EntityManager manager;
 
+	@Override
 	public User authorizeLogin(String email, String password) {
-		return null;
+		User user = null;
+		Query query = manager.createQuery("SELECT u FROM User u WHERE u.email = :userEmail AND "
+				+ "u.password = :userPassword AND u.category = :userCategory ");
+		query.setParameter("userEmail", email);
+		query.setParameter("userPassword", password);
+		query.setParameter("userCategory", true);	// administrators only 
+		
+		try {
+			user = (User) query.getSingleResult();
+		}catch (NoResultException e) {
+			user = null;
+		}		
+		return user;
 	}
 
-	
+
 }
