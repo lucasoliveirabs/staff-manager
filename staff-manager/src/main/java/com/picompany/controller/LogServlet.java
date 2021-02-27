de573d3170e083de9648d8553c0d5775d7663b78
Lucas Oliveira <lucasoliveirabs95@gmail.com>
2021-02-27 19:17:37 -0300

    create 'LogServlet' and inject a 'UserDao' bean in a 'UserDaoLocal'

---
+++ staff-manager/src/main/java/com/picompany/controller/LogServlet.java
@@ -0,0 +1,32 @@
+package com.picompany.controller;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.picompany.dao.UserDaoLocal;
+import com.picompany.model.User;
+
+@WebServlet(urlPatterns = "/log/*", name = "log")
+public class LogServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private UserDaoLocal dao;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	
+}
