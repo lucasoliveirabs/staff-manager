57bf2c21a04365194d7ab872a9db1e84277d3262
Lucas Oliveira <lucasoliveirabs95@gmail.com>
2021-02-27 21:29:51 -0300

    add to 'SessionFilter' security redirecting in case of user logout

--- staff-manager/src/main/java/com/picompany/filters/SessionFilter.java
+++ staff-manager/src/main/java/com/picompany/filters/SessionFilter.java
@@ -8,6 +8,9 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebFilter("*.jsp")
 public class SessionFilter implements Filter {
@@ -19,7 +22,20 @@ public class SessionFilter implements Filter {
 
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
 			throws IOException, ServletException {
-		
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		String uri = request.getRequestURI();
+		HttpSession session = request.getSession();
+
+		boolean isNotLogged = session.getAttribute("user") == null;
+		boolean isSecurityFile = (uri.contains("login") == false || uri.contains("new-account") == false);
+
+		if (isSecurityFile && isNotLogged) {
+			response.sendRedirect("login.jsp");
+			// send message:
+			// 'You must login as an administrator user to access the system.'
+		}
+		chain.doFilter(request, response);
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
