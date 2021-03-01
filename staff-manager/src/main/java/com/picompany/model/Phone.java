8e39b525a972d8a56d24b775d8da67a0bf0cc94a
Lucas Oliveira <lucasoliveirabs95@gmail.com>
2021-03-01 08:30:45 -0300

    fix grammar

--- staff-manager/src/main/java/com/picompany/model/Phone.java
+++ staff-manager/src/main/java/com/picompany/model/Phone.java
@@ -36,12 +36,12 @@ public class Phone {
 	private int ddd;
 	
 	@Size(min = 8, max = 9, message = "The phone number length must be between 8 and 9 numbers")
-	@NotEmpty(message = "The phone number cannot empty or null")
+	@NotEmpty(message = "The phone number cannot be empty or null")
 	@Pattern(regexp = "^[0-9]+$", message = "The phone number has invalid characters")
 	@Column(nullable = false, unique = true, length = 9)
 	private String number;
 	
-	@NotEmpty(message = "The phone type cannot empty or null")
+	@NotEmpty(message = "The phone type cannot be empty or null")
 	@Column(nullable = false, length = 20)
 	private String type;
 	
