3db9ea097e058c5e9e19d1d591e41f6f8514ffaa
Lucas Oliveira <lucasoliveirabs95@gmail.com>
2021-03-01 22:17:08 -0300

    add 'verifyEntryParameters()' support method to UserServlet

--- staff-manager/src/main/java/com/picompany/controller/UserServlet.java
+++ staff-manager/src/main/java/com/picompany/controller/UserServlet.java
@@ -1,6 +1,7 @@
 package com.picompany.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.picompany.dao.UserDaoLocal;
+import com.picompany.model.Phone;
 import com.picompany.model.User;
 
 @WebServlet(urlPatterns = "/users/*", name = "users")
@@ -34,8 +36,11 @@ public class UserServlet extends HttpServlet {
 			case "get":
 				getAllUsers(request, response);
 				break;
-			case "load":
-				loadUser(request, response);
+			case "loadforview":
+				loadUserView(request, response);
+				break;
+			case "loadforupdate":
+				loadUserUpdate(request, response);
 				break;
 			case "delete":
 				deleteUser(request, response);
@@ -80,30 +85,74 @@ public class UserServlet extends HttpServlet {
 	}
 
 	private void postUser(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+			throws Exception {
+		String email = request.getParameter("email");
+		User existingUser = dao.getUserByEmail(email);
 
-		User user = new User();		
-		user.setName(request.getParameter("name"));
-		user.setEmail(request.getParameter("email"));
-		user.setPassword(request.getParameter("password"));
-		user.setCategory(Boolean.valueOf(request.getParameter("category")));
-		// verifyParameters(HttpServletRequest request, User user);
-		
-		dao.postUser(user);
-		
-		HttpSession session = request.getSession(false);		
-		if (session.getAttribute("user") != null) {
-			//send message:
-			//The new collaborator is registered successfully
-			getAllUsers(request, response);
+		if (existingUser == null) {
+			User user = verifyEntryParameters(request, new User());
+			dao.postUser(user);
 		} else {
-			//send message:
-			//The account is successfully created 
+			response.sendRedirect("new-account.jsp");
+			// send message:
+			// "This e-mail is already in use."
+		}
+
+		HttpSession session = request.getSession(false);
+		if (session.getAttribute("user") == null) {
+			// send message:
+			// The account is successfully created.
 			response.sendRedirect("login.jsp");
+		} else {
+			// send message:
+			// The new collaborator is successfully registered.
+			getAllUsers(request, response);
 		}
 	}
 
-	private void loadUser(HttpServletRequest request, HttpServletResponse response)
+	private void putUser(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		Long id = Long.parseLong(request.getParameter("id"));
+		User user = dao.getUserById(id);
+
+		user = verifyEntryParameters(request, user);
+
+		dao.putUser(user);
+		getAllUsers(request, response);
+		// send message:
+		// The collaborator is successfully updated.
+	}
+
+	private void deleteUser(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Long id = Long.parseLong(request.getParameter("id"));
+
+		User deletedUser = dao.getUserById(id);
+		dao.deleteUser(id);
+
+		HttpSession session = request.getSession();
+		User loggedInUser = (User) session.getAttribute("user");
+
+		if (deletedUser.getId() == loggedInUser.getId()) {
+			response.sendRedirect("log");
+		} else {
+			getAllUsers(request, response);
+			// send message:
+			// The collaborator is successfully deleted.
+		}
+	}
+
+	private void loadUserView(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Long id = Long.parseLong(request.getParameter("id"));
+		User user = dao.getUserById(id);
+
+		request.setAttribute("user", user);
+		RequestDispatcher rd = request.getRequestDispatcher("view-collab.jsp");
+		rd.forward(request, response);
+	}
+
+	private void loadUserUpdate(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		Long id = Long.parseLong(request.getParameter("id"));
 		User user = dao.getUserById(id);
@@ -113,30 +162,48 @@ public class UserServlet extends HttpServlet {
 		rd.forward(request, response);
 	}
 
-	private void putUser(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		Long id = Long.parseLong(request.getParameter("id"));
-		User user = dao.getUserById(id);
-		// verifyParameters(HttpServletRequest request, User user);
+	private User verifyEntryParameters(HttpServletRequest request, User user) throws Exception {
+		user.setName(request.getParameter("name"));
+		user.setEmail(request.getParameter("email"));
+		user.setPassword(request.getParameter("password"));
+		user.setCategory(Boolean.valueOf(request.getParameter("category")));
 		
-		dao.putUser(user);
-		getAllUsers(request, response);
-	}
-
-	private void deleteUser(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		Long id = Long.parseLong(request.getParameter("id"));
+		String ddd = null;
+		String number = null;
+		String type = null;
 		
-		User deletedUser = dao.getUserById(id);
-		dao.deleteUser(id);
-		
-		HttpSession session = request.getSession();
-		User loggedInUser = (User) session.getAttribute("user");
-
-		if (deletedUser.getId() == loggedInUser.getId()) {
-			response.sendRedirect("log");
-		} else {
-			getAllUsers(request, response);	
+		List<Phone> phones = new ArrayList<Phone>();
+		for(int i = 0; i<3; i++) {
+			ddd = request.getParameter("ddd"+String.valueOf(i));
+			number = request.getParameter("number"+String.valueOf(i));
+			type = request.getParameter("type"+String.valueOf(i));
+			
+			boolean dddIsNotValid = (ddd == null || ddd.isEmpty());
+			boolean numberIsNotValid = (number == null || number.isEmpty() ||
+					(number.length() < 8 || number.length() > 9));
+			boolean typeIsNotValid = (type == null || type.isEmpty());
+			
+			if (dddIsNotValid || numberIsNotValid || typeIsNotValid) {
+				break;
+			}
+			
+			int convertedDdd = 0;
+			try {
+				convertedDdd = Integer.parseInt(ddd);
+			} catch (NumberFormatException e) {
+				throw new Exception("Invalid DDD.");
+			}
+						
+			Phone phone = new Phone();
+			phone.setDdd(convertedDdd);
+			phone.setNumber(number);
+			phone.setType(type);
+			phones.add(phone);
 		}
+		
+		user.setPhones(phones);
+		
+		return user;
+
 	}
 }
